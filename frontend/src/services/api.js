b2894536d6b4f70d95998461a4e0d81991d07ddd
Aman-webDev-2025 <mr.aman.kr2025@gmail.com>
2026-04-27 17:20:12 +0530

    fixed backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://visitor-pass-management-system-sw19.onrender.com";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://visitor-pass-management-system-sw19.onrender.com/api";
 
 const API = axios.create({
   baseURL: API_BASE_URL,
